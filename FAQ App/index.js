03a2338ce85c78396452482be9aef91dbdada3ad
RaFi <109336742+frontend-rafi@users.noreply.github.com>
2023-01-12 17:19:59 -0300

    Add files via upload

---
+++ FAQ App/index.js
@@ -0,0 +1,14 @@
+const faqs = document.querySelectorAll('.faq');
+
+
+
+faqs.forEach(function(faq) {
+    faq.addEventListener('click', function(){
+    faq.classList.toggle("active");
+    })
+    
+})
+
+
+
+
